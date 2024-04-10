9878de47de195a6ca2650b29fe459fbcd9771c81
Luiz Roberto <luizrobertoao29@gmail.com>
2024-04-09 22:35:03 -0300

    Creating validation methods at PasswordValidator.

--- src/main/java/com/itaubackendchallenge/paswordvalidator/validator/PasswordValidator.java
+++ src/main/java/com/itaubackendchallenge/paswordvalidator/validator/PasswordValidator.java
@@ -3,12 +3,85 @@ package com.itaubackendchallenge.paswordvalidator.validator;
 import com.itaubackendchallenge.paswordvalidator.model.Password;
 import org.springframework.stereotype.Component;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Component
 public class PasswordValidator {
 
     public boolean isValid(Password password) {
-        //implement validation
+
+        String passwordValue = password.getValue();
+
+        if (passwordValue.length() < 9) {
+            return false;
+        }
+        if (!hasDigit(passwordValue)) {
+            return false;
+        }
+        if (!hasLowerCase(passwordValue)) {
+            return false;
+        }
+        if (!hasUpperCase(passwordValue)) {
+            return false;
+        }
+        if (!hasSpecialCharacter(passwordValue)) {
+            return false;
+        }
+        if (hasRepeatedCharacters(passwordValue)) {
+            return false;
+        }
+        if (passwordValue.contains(" ")) {
+            return false;
+        }
         return true;
+    }
+
+    private boolean hasDigit(String str) {
+        for (char c : str.toCharArray()) {
+            if (Character.isDigit(c)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean hasLowerCase(String str) {
+        for (char c : str.toCharArray()) {
+            if (Character.isLowerCase(c)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean hasUpperCase(String str) {
+        for (char c : str.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean hasSpecialCharacter(String str) {
+        String specialCharacters = "!@#$%^&*()-+";
+        for (char c : str.toCharArray()) {
+            if (specialCharacters.contains(String.valueOf(c))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean hasRepeatedCharacters(String str) {
+        Set<Character> seen = new HashSet<>();
+        for (char c : str.toCharArray()) {
+            if (!seen.add(c)) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
