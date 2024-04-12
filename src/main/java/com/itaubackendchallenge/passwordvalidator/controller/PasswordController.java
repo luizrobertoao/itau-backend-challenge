77cc487e09dafd0623b0c76b2e639e1d01b939a7
Luiz Roberto <luizrobertoao29@gmail.com>
2024-04-12 08:35:49 -0300

    refactor: Cleaning unused imports.

--- src/main/java/com/itaubackendchallenge/passwordvalidator/controller/PasswordController.java
+++ src/main/java/com/itaubackendchallenge/passwordvalidator/controller/PasswordController.java
@@ -1,17 +1,18 @@
 package com.itaubackendchallenge.passwordvalidator.controller;
 
-import com.itaubackendchallenge.passwordvalidator.exception.PasswordInvalidException;
 import com.itaubackendchallenge.passwordvalidator.model.PasswordRequest;
 import com.itaubackendchallenge.passwordvalidator.service.PasswordService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "Password Validator API", description = "API to validate passwords.")
 
